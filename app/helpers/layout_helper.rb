99465bd1030a9ab7efa5f20332be9ac4bf20078b
marc <marc@bc82c6ac-daf3-4a72-9b3c-baec5c277ba7>
2009-06-30 21:05:15 +0000

    Removed js helper method from layout helper

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -3,13 +3,7 @@
 # This module should be included in all views globally,
 # to do so you may need to add this line to your ApplicationController
 #   helper :layout
-module LayoutHelper
-
-  class JS
-    cattr_accessor :js_includes
-    @@js_includes = {}
-  end
-  
+module LayoutHelper  
   def title(page_title, show_title = true)
     @content_for_title = page_title.to_s
     @show_title = show_title
@@ -52,7 +46,7 @@ module LayoutHelper
   # asset inclusion helpers
   def use_orig_lightview
     stylesheet '/javascripts/lightview2.5/css/lightview.css'
-    js_include prototype, scriptaculous, 'lightview2.5/js/lightview'
+    javascript prototype, scriptaculous, 'lightview2.5/js/lightview'
   end
   
   def use_lightview
@@ -61,78 +55,78 @@ module LayoutHelper
   end
   
   def use_lightview_only_js
-    js_include prototype, scriptaculous, "lightview/js/lightview" 
+    javascript prototype, scriptaculous, "lightview/js/lightview" 
   end
   
   def use_jquery
-    js_include 'jQuery/jquery-1.2.6', 'jQuery/jquery-ui'
+    javascript 'jQuery/jquery-1.2.6', 'jQuery/jquery-ui'
   end
   
   def use_scrollable
     use_jquery
-    js_include 'jQuery/jquery.scrollable-1.0.1', 'jQuery/jquery.mousewheel', 'scrollable', 'slideshow'
+    javascript 'jQuery/jquery.scrollable-1.0.1', 'jQuery/jquery.mousewheel', 'scrollable', 'slideshow'
     stylesheet 'scrollable', 'scrollable-navig'
   end
   
   def use_flash
-    js_include 'swfobject'
+    javascript 'swfobject'
   end
   
   def use_flashplayer
-    js_include 'flowplayer-3.0.0-rc5.min', '/javascripts/flowplayer.js'
+    javascript 'flowplayer-3.0.0-rc5.min', '/javascripts/flowplayer.js'
   end
   
   def use_js_cookies
-    js_include 'cookiejar', 'application'
+    javascript 'cookiejar', 'application'
   end
   
   def use_lowpro
-    js_include prototype, scriptaculous, 'lowpro', 'defaultvalueactsashint', 'behaviors'
+    javascript prototype, scriptaculous, 'lowpro', 'defaultvalueactsashint', 'behaviors'
   end
   
   def use_prototip
     use_lowpro
-    js_include 'prototip'
+    javascript 'prototip'
     stylesheet 'prototip'
   end
   
   def use_form
     use_lowpro
-    js_include 'CustomFormElements'
+    javascript 'CustomFormElements'
     use_busy
   end
   
   def use_tabs
-    js_include "http://api.maps.yahoo.com/v3.0/fl/javascript/apiloader.js?appid=#{YAHOO_APP_ID}", 
+    javascript "http://api.maps.yahoo.com/v3.0/fl/javascript/apiloader.js?appid=#{YAHOO_APP_ID}", 
       'tabs', prototype
     stylesheet 'tabs'
   end
   
   def use_busy
-    js_include 'application', 'cvi_busy_lib'
+    javascript 'application', 'cvi_busy_lib'
   end
   
   def use_wysiwyg
-    js_include 'fckeditor/fckeditor', 'wysiwyg'
+    javascript 'fckeditor/fckeditor', 'wysiwyg'
 #      'http://yui.yahooapis.com/2.7.0/build/yahoo/yahoo-min.js',
 #      'http://yui.yahooapis.com/2.7.0/build/get/get-min.js'
   end
   
   def use_accordion
-    js_include scriptaculous, 'accordion'
+    javascript scriptaculous, 'accordion'
     stylesheet 'accordion'
   end
   
   def use_calendar
-    js_include 'datepicker'
+    javascript 'datepicker'
     stylesheet 'datepicker'
   end
   
   def use_soundmanager
     content_for(:javascript) { "\n<!-- SM2 JS START -->\n"}
-    js_include (RAILS_ENV == 'development') ? 'soundmanager2' : 'soundmanager2-nodebug-jsmin'
-    #js_include 'page-player'
-    js_include 'inlineplayer'
+    javascript (RAILS_ENV == 'development') ? 'soundmanager2' : 'soundmanager2-nodebug-jsmin'
+    #javascript 'page-player'
+    javascript 'inlineplayer'
     #content_for(:javascript) { javascript_tag "soundManager.url = '/swf'" }
     content_for(:javascript) { "\n<!-- SM2 JS END -->\n"}
     stylesheet 'soundmanager2', 'inlineplayer'
@@ -147,13 +141,13 @@ module LayoutHelper
   def use_autocomplete
     use_jquery
     stylesheet 'jquery.autocomplete'
-    js_include 'jQuery/jquery.autocomplete', 'application_jquery'
+    javascript 'jQuery/jquery.autocomplete', 'application_jquery'
   end
   
   def use_uploader
     @no_prototype = true
     stylesheet "content_upload"
-    js_include "mootools-1.2-core-nc", "Swiff.Uploader", "Fx.ProgressBar", "FancyUpload2", "upload_form"
+    javascript "mootools-1.2-core-nc", "Swiff.Uploader", "Fx.ProgressBar", "FancyUpload2", "upload_form"
   end
   
   def style_hidden_if(expression)
@@ -162,16 +156,4 @@ module LayoutHelper
   
   private
   
-  # Helper to prevent duplicate includes of javascript files 
-  def js_include(*args)
-    js = []
-    args.each do |a| 
-      unless JS.js_includes.key? a
-        js << a
-        JS.js_includes[a] = true
-      end
-    end
-    javascript *js
-  end
-
 end
