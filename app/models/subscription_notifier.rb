914f0eabc61be0c2d0070d72bf846c3f91cca94c
dimas <dimas@bc82c6ac-daf3-4a72-9b3c-baec5c277ba7>
2009-08-24 08:00:25 +0000

    fix Password Reset Email

--- app/models/subscription_notifier.rb
+++ app/models/subscription_notifier.rb
@@ -6,6 +6,7 @@ class SubscriptionNotifier < ActionMailer::Base
     @subject = subject
     @recipients = to.respond_to?(:email) ? to.email : to
     @from = from.respond_to?(:email) ? from.email : from
+    @content_type = "text/html"
   end
   
   def welcome(account)
