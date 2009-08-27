68dc57236e844083b4f22933887569b17774977d
marc <marc@bc82c6ac-daf3-4a72-9b3c-baec5c277ba7>
2009-08-27 22:46:03 +0000

    Resolves #239

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   before_filter :load_facebook_connect
   before_filter :set_facebook_session 
   before_filter :redirect_if_logged_in, :only => :index
-  layout :dynamic_layout
+  layout 'public'
   
   def new
     @user = User.new
@@ -17,17 +17,9 @@ class HomeController < ApplicationController
     render :action => params[:page]
   end
   
-  def test_lightview
-    # WHY DOES :layout => 'empty' NOT WORK??? GAHHH!
-    render :layout => 'empty' #, :layout => 'empty'
-  end
   
   private
-  
-  def dynamic_layout
-    super unless params[:page] == 'test_lightview_og'
-  end
-    
+      
   def redirect_if_logged_in
     # Redirect to dashboard for logged in sessions unless user is coming 
     # from site link
