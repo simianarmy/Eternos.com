9a588849fda11bb311e9912187fc6ea0bc89a9a3
marc <marc@bc82c6ac-daf3-4a72-9b3c-baec5c277ba7>
2009-09-19 23:12:51 +0000

    crazy hacks for AS UI

--- app/controllers/account_settings_controller.rb
+++ app/controllers/account_settings_controller.rb
@@ -19,13 +19,17 @@ class AccountSettingsController < ApplicationController
     when 1
       load_online
       'online'
-    when 2, 3
+    when 2,3
       load_email_accounts
       'email_account'
+    # when 3
+    #      load_history
+    #      'your_history'
     else
       'personal_info'
     end
-
+    @active_step = [@completed_steps+1, 3].min
+    
     respond_to do |format|
       format.html
       format.js
