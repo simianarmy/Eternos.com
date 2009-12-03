703758e4954de8ccee152a7c40ab330d1812454d
marc <marc@bc82c6ac-daf3-4a72-9b3c-baec5c277ba7>
2009-12-03 07:35:40 +0000

    Removing frozen gems in preparation for Bundler

--- vendor/gems/rvideo-0.9.3/lib/rvideo/float.rb
+++
@@ -1,7 +0,0 @@
-# Add a rounding method to the Float class.
-class Float
-  def round_to(x)
-    (self * 10**x).round.to_f / 10**x
-  end
-end
-
--- vendor/gems/rvideo-0.9.3/spec/integrations/transcoding_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + "/../spec_helper"
-
-module RVideo
-  describe RVideo do
-    before do
-      
-    end
-  end
-end
