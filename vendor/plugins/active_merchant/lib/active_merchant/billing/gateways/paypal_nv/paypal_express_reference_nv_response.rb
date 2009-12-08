aab5b105e7a31ca5d6ce9d81fba799f5a5d53e99
marc <marc@bc82c6ac-daf3-4a72-9b3c-baec5c277ba7>
2009-12-08 22:38:11 +0000

    removing active_merchant in order to install latest saas software copy

--- vendor/plugins/active_merchant/lib/active_merchant/billing/gateways/paypal_nv/paypal_express_reference_nv_response.rb
+++
@@ -1,9 +0,0 @@
-module ActiveMerchant #:nodoc:
-  module Billing #:nodoc:
-    class PaypalExpressReferenceNvResponse < PaypalExpressNvResponse
-      def token
-        @params['billingagreementid'] || @params['token']
-      end
-    end
-  end
-end
