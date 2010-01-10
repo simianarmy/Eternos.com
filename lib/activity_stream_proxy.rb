83686bf3ae0cfef5ad7f516ebbdba205a40b07c3
marc <marc@bc82c6ac-daf3-4a72-9b3c-baec5c277ba7>
2010-01-10 21:24:33 +0000

    Updated actitity stream proxy class

--- lib/activity_stream_proxy.rb
+++ lib/activity_stream_proxy.rb
@@ -5,24 +5,9 @@
 
 # See facebook_activity.rb for usage example
 
-# TODO: use Mash/Hashie?
 class ActivityStreamProxy < Hashie::Mash
-#  attr_accessor :id, :author, :created, :updated, :message, :source_url, :attribution, :type, :attachment_type
-  
-  # All this is overkill now that we are using Hashie...
-  class Attachment
-    attr_reader :data
-    
-    def initialize(data)
-      @data = data 
-    end
-  end
-  
-  def attachment=(data)
-    @attachment = Attachment.new(data)
-  end
-  
+  # TODO: For backwards compat - update client code
   def attachment_data
-    @attachment ? @attachment.data : nil
+    attachment
   end
 end
