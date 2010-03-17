cb48997134745967aead9570886c5c2354521ff2
marc <marc@bc82c6ac-daf3-4a72-9b3c-baec5c277ba7>
2010-03-17 05:34:59 +0000

    Added clearboth class to members.css to fix details view

--- app/helpers/tooltips_helper.rb
+++ app/helpers/tooltips_helper.rb
@@ -4,4 +4,9 @@ module TooltipsHelper
   def default_prototip_options
     "hook: {target: 'topRight', tip: 'bottomLeft'}, stem: 'bottomLeft'"
   end
+  
+  # Generates custom Prototip step/hook options string in JSON format
+  def prototip_options_for_div(div)
+    "{target: $('#{div}'), hook: {target: 'topMiddle', tip: 'bottomMiddle'}, stem: 'bottomLeft'}"
+  end
 end
