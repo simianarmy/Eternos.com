de02079a8e7e981cded840c0307d94a9f848cde9
marc <marc@bc82c6ac-daf3-4a72-9b3c-baec5c277ba7>
2010-06-06 23:50:11 +0000

    Have to keep tmp dir out of scm for shit to work

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -5,25 +5,28 @@
 
 # UNCOMMENT BELOW WHEN Bundler 0.9+ ACTUALLY WORKS
 
-# TRY THIS FOR PASSENGER
+# WORKAROUND FOR CUSTOM APPS LOADING RAILS THAT ALREADY HAVE BUNDLER LOADED
+unless defined? Bundler
+  
+  begin
+    require "rubygems"
+    require "bundler"
+  rescue LoadError
+    raise "Could not load the bundler gem. Install it with `gem install bundler`."
+  end
 
-begin
-  require "rubygems"
-  require "bundler"
-rescue LoadError
-  raise "Could not load the bundler gem. Install it with `gem install bundler`."
-end
+  if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.20")
+    raise RuntimeError, "Your bundler version is too old." +
+     "Run `gem install bundler` to upgrade."
+  end
 
-if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.20")
-  raise RuntimeError, "Your bundler version is too old." +
-   "Run `gem install bundler` to upgrade."
-end
-
-begin
-  # Set up load paths for all bundled gems
-  ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
-  Bundler.setup
-rescue Bundler::GemNotFound
-  raise RuntimeError, "Bundler couldn't find some gems." +
-    "Did you run `bundle install`?"
-end
+  begin
+    # Set up load paths for all bundled gems
+    ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
+    Bundler.setup
+  rescue Bundler::GemNotFound
+    raise RuntimeError, "Bundler couldn't find some gems." +
+      "Did you run `bundle install`?"
+  end
+  
+end # defined? DaemonKit
