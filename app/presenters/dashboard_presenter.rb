5efd826be37d02f0a5d1def7375b84112e3856ff
Marc A. Mauger <simianarmy@gmail.com>
2010-09-28 22:10:12 -0700

    More work on dashboard backup sites info

--- app/presenters/dashboard_presenter.rb
+++ app/presenters/dashboard_presenter.rb
@@ -14,7 +14,23 @@ class DashboardPresenter < Presenter
     @days_signed_up = (Date.current - @user.created_at.to_date + 1).to_i.to_s
     @birthday = @user.profile.birthday.to_date rescue ''
     @account_state = user.state.to_s.capitalize
-    @backup_sites = user.backup_sources
+    @backup_sites = user.backup_sources.map do |bs|
+      status = if bs.active?
+        'OK'
+      elsif auth_required?
+        'Authentication Required'
+      elsif backup_broken?
+        'Too many failed attempts'
+      else
+        'Inactive'
+      end
+      # Return simple hash with backup source info
+      {:description => bs.description,
+        :created_at => bs.created_at.to_date,
+        :last_backup_at => (bs.latest_backup ? bs.latest_backup.finished_at : bs.last_backup_at).to_date,
+        :status => status}
+    end
+    Rails.logger.debug "backup sites: #{@backup_sites.inspect}"
   end
   
   def backup_data_counts
